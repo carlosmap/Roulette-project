c8558eb085a955813ce6fa316bb58cde0a7a9d68
Carlosmap <carlosmap@gmail.com>
2020-12-21 19:58:35 -0600

    Roulette project folder configured with initial modules

---
+++ Roulette/src/test/java/com/Project/Roulette/RouletteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Project.Roulette;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RouletteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
